cd66dbf041a38885138ca8fb8176ebf605cfa57d
Aditya <162988821+invincibleAditya303@users.noreply.github.com>
2024-11-19 11:38:37 +0530

    Update CartContext.js
    Adding Popup context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const CartContext = React.createContext({
   cartList: [],
+  paymentMethod: '',
+  displayMessage: false,
   removeAllCartItems: () => {},
   addCartItem: () => {},
   removeCartItem: () => {},
   incrementCartItemQuantity: () => {},
   decrementCartItemQuantity: () => {},
+  onChangePaymentMethod: () => {},
+  onClickOrderButton: () => {},
 })
 
 export default CartContext
